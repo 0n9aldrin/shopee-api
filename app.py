7ad9a9cccac9a2b86852da5331d1c3a976a39ff8
0n9aldrin <0n9aldrin@gmail.com>
2020-05-08 16:31:07 +0700

    import selenium

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hi!'
+    url = "https://www.bukalapak.com/products?utf8=✓&source=navbar&from=omnisearch&search_source=omnisearch_organic&from_keyword_history=false&search%5Bkeywords%5D=calculator"
+    r = requests.get(url)
+    return r.content
 
 
 if __name__ == "__main__":
