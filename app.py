60c051ad5be84cb0262a4a074551347a40c34d52
0n9aldrin <0n9aldrin@gmail.com>
2020-05-09 22:42:25 +0700

    change get method

--- app.py
+++ app.py
@@ -5,33 +5,16 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-STUDENTS = {
-  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
-  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
-  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
-  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
-}
 
 parser = reqparse.RequestParser()
 
 class StudentsList(Resource):
     def get(self):
-        return STUDENTS
-    def post(self):
-        parser.add_argument("name")
-        parser.add_argument("age")
-        parser.add_argument("spec")
+        parser.add_argument("keyword")
         args = parser.parse_args()
-        student_id = int(max(STUDENTS.keys())) + 1
-        student_id = '%i' % student_id
-        STUDENTS[student_id] = {
-            "name": args["name"],
-            "age": args["age"],
-            "spec": args["spec"],
-        }    
-        return STUDENTS[student_id], 201
+        return args["keyword"]
 
-api.add_resource(StudentsList, '/students/')
+api.add_resource(StudentsList, '/api/')
 
 
 if __name__ == "__main__":
