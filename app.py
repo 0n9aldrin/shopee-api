87343ef09f887e7c5dba662c8c545c6d7f05eb2b
0n9aldrin <0n9aldrin@gmail.com>
2020-05-11 01:36:52 +0700

    Add page argument

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def getShopee(keyword,page):
             'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
             'cookie': '_gcl_au=1.1.462705111.1588794535; _fbp=fb.2.1588794535066.447793156; SPC_IA=-1; SPC_EC=-; SPC_U=-; SPC_F=msmAArgoZQQynd8y4m8zFGqOAOrFv02z; REC_T_ID=9e6f0c7e-8fd2-11ea-a4b9-ccbbfe5de5ff; SPC_SI=98bhb0q6ym7fbej4utjk4euft93n43sf; AMP_TOKEN=%24NOT_FOUND; _ga=GA1.3.623474330.1588794537; _gid=GA1.3.1283352780.1588794537; SPC_CT_c312ba2b="1588794565.Nlp8wfA3Ir+Zx2s5thu3KHxfkIzRQgpjCnfQ/GC1sJ4="; SPC_R_T_ID="h5sEB8bcl/05iuSfyphx4JGe1GU4SGMajYQKEeKMntGP8JaaY7eoXgQ4XUy02xna51PTsWPYANfzh36A5NY7KQrL+l23EnrWV8VgRyNCJ2s="; SPC_T_IV="beQldInTVFNRjcAxuMn3bg=="; SPC_R_T_IV="beQldInTVFNRjcAxuMn3bg=="; SPC_T_ID="h5sEB8bcl/05iuSfyphx4JGe1GU4SGMajYQKEeKMntGP8JaaY7eoXgQ4XUy02xna51PTsWPYANfzh36A5NY7KQrL+l23EnrWV8VgRyNCJ2s="',
         }
-        page = page * 50
+        page = int(page) * 50
         params = (
             ('by', 'relevancy'),
             ('keyword', keyword),
